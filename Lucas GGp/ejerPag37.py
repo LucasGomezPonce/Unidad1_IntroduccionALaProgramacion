46313cc33cae63a2e286be3bb7674b977f0c19a9
Lucas <lggp_1312@hotmail.com>
2025-04-10 17:26:45 -0300

    subiendo primeros ejercicios

---
+++ Lucas GGp/ejerPag37.py
@@ -0,0 +1,7 @@
+"""
+Modulo programador - Eje1 Python introduccion pagina 35, en pdf pagina 37
+1- Escribe un programa ue pida al usuario el readio de un circulo y calcule el area
+2- Escribe un programa que convierta una temperatura dada en grados Celcius a grados Fahrenheit.
+3- Escribe un programa que pida al usuario los dos catetos de un triangulo y calcule la hipotenusa
+4- Escribe un programa que pida al usuario su año de nacimiento, calcule su edad y genere un mensaje de saludo personalizado que incluya su nomvre y la edad calculada.
+"""
