f4b1628037415b11040734e767fedd8d2333628f
Lucas <lggp_1312@hotmail.com>
2025-04-11 17:52:22 -0300

    asdasdasd

--- Lucas GGp/ejerPag37.py
+++ Lucas GGp/ejerPag37.py
@@ -3,9 +3,9 @@ Modulo programador - Eje1 Python introduccion pagina 35, en pdf pagina 37
 1- Escribe un programa que pida al usuario el radio de un circulo y calcule el area
 2- Escribe un programa que convierta una temperatura dada en grados Celcius a grados Fahrenheit.
 3- Escribe un programa que pida al usuario los dos catetos de un triangulo y calcule la hipotenusa
-4- Escribe un programa que pida al usuario su año de nacimiento, calcule su edad y genere un mensaje de saludo personalizado que incluya su nomvre y la edad calculada.
+4- Escribe un programa que pida al usuario su año de nacimiento, calcule su edad y genere un mensaje de saludo personalizado que incluya su nombre y la edad calculada.
 """
-
+# Ejer 01
 import math
 
 radio = int(input('Ingresa el radio de un circulo: '))
@@ -13,3 +13,9 @@ radio = int(input('Ingresa el radio de un circulo: '))
 area = math.pi * ((radio)**2)
 
 print(area)
+
+# Ejer 02
+
+print("Hola")
+
+
