cb95934104f42a508d9abf453b629bfe19da2e49
Lucas <lggp_1312@hotmail.com>
2025-04-16 20:44:12 -0300

    desafio04

--- Lucas GGp/desafios.py
+++ Lucas GGp/desafios.py
@@ -56,4 +56,15 @@ def desafio03():
 
 
 def desafio04():
-    pass
+    contador = 0
+    while True:
+
+        tiempo = int(input("Ingrese el tiempo del tramo: "))
+        if tiempo == 0:
+            break
+        contador += tiempo
+
+    horas = contador // 60
+    minutos = contador % 60
+
+    print(f"El tiempo total del viaje es:{horas}:{minutos}")
